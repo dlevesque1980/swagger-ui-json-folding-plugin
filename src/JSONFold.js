434b833a222d494d49aeadd113245fe53de8c679
dlevesque1980 <didier.levesque@gmail.com>
2017-10-28 09:51:30 -0400

    Fix for no json
    when no json. The component render with pre tag.

--- src/JSONFold.js
+++ src/JSONFold.js
@@ -3,13 +3,29 @@ import PropTypes from "prop-types"
 import ReactJson from 'react-json-view'
 
 export default class JSONFold extends Component {
+
+  initializeComponent = (c) => {
+    this.el = c
+  }
+
   render() {
-    let { value } = this.props
-    const data = JSON.parse(value);
-    return <ReactJson src={data} theme="monokai" name={false} />
+    let { value, className } = this.props
+    className = className || ""
+    let data = null;  
+    try {
+      data = JSON.parse(value);
+    } catch(e) {
+      data = null;
+    }
+    if (data) {
+      return <ReactJson src={data} theme="monokai" name={false} />
+    }
+
+    return <pre ref={this.initializeComponent} className={className + " microlight"}>{value}</pre>
   }
 }
 
 JSONFold.propTypes = {
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
+  className: PropTypes.string
 }
