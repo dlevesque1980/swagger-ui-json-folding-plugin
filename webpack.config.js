3b07a3865d0fe97b1f5625e4cf47f51d251da60e
Didier Levesque <didier.levesque@gmail.com>
2019-01-05 09:32:35 -0500

    Fix vunerability and pass to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: {
         "swagger-ui-json-folding-plugin" : "./src/index.js"
     },
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'       
-        }]
+        rules: [{
+			test: /\.js?$/,
+			oneOf: [{            
+				exclude: /node_modules/,
+				loader: 'babel-loader'       
+			}]
+		}],
     },
     resolve: {
         extensions: ['.js', '.jsx']
     },
+	optimization: {
+		minimizer: [new UglifyJsPlugin({
+              cache: true,
+              parallel: true,
+              sourceMap: true
+            })],
+	},	
+	devtool: 'source-map',
     output: {
-        path: __dirname + '/demo',
+        path: __dirname + '/dist',
         publicPath: '/',
         filename: '[name].js',
         library: 'jsonFoldingPlugin',
