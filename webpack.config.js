ced1b9c411cfb683ae536211d9ce2ef351927a42
Didier Levesque <didier.levesque@gmail.com>
2020-08-14 07:27:13 -0400

    fix dependencies security

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,36 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: {
-        "swagger-ui-json-folding-plugin" : "./src/index.js"
-    },
-    module: {
-		rules: [
-		  {
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: {
-			  loader: "babel-loader"
-			}
-		  }
-		]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-	optimization: {
-		minimizer: [new UglifyJsPlugin({
-              cache: true,
-              parallel: true,
-              sourceMap: true
-            })],
-	},	
-	devtool: 'source-map',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: '[name].js',
-        library: 'jsonFoldingPlugin',
-    },
-    devServer: {
-        contentBase: './demo',
-		port:9000
-    }
+  entry: {
+    "swagger-ui-json-folding-plugin": "./src/index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: '[name].js',
+    library: 'jsonFoldingPlugin',
+  },
+  devServer: {
+    contentBase: './demo',
+    port: 9000
+  }
 };
